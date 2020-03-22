d9626d8765ca4306a74a2094ebdb437ea05d51d2
Pasha Sheholay <pavlik198926@gmail.com>
2020-03-22 19:17:43 +0300

    Add some lines of 'println'

--- startjava/lesson_1/Variable.java
+++ startjava/lesson_1/Variable.java
@@ -3,10 +3,19 @@ class Variable {
         byte memoryRAM = 8;
         short systemType = 64;
         int windows = 10;
-        long productID = 389_834_822_343_347_888;
+        long productID = 389834822;
         float frequencyCPU = 3.4f;
         double timeOfWork = 3.93874893797439d;
         char modelOfCPU = 'R';
         boolean isWindows = true;
+
+        System.out.println("Memory (RAM) - " + memoryRAM);
+        System.out.println("System type  - " + systemType);
+        System.out.println("Computer system - " + windows);
+        System.out.println("Product key - " + productID);
+        System.out.println("Frequency of CPU - " + frequencyCPU);
+        System.out.println("Working of hours - " + timeOfWork);
+        System.out.println("Model of CPU - " + modelOfCPU);
+        System.out.println("Is it Windows - " + isWindows);
     }
 }

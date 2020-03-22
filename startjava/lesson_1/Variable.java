d119b67b68b019d61219f93fadda038fdd56cca4
Pasha Sheholay <pavlik198926@gmail.com>
2020-03-22 18:57:36 +0300

    Add file <Variable>

---
+++ startjava/lesson_1/Variable.java
@@ -0,0 +1,12 @@
+class Variable {
+    public static void main(String[] args) {
+        byte memoryRAM = 8;
+        short systemType = 64;
+        int windows = 10;
+        long productID = 389_834_822_343_347_888;
+        float frequencyCPU = 3.4f;
+        double timeOfWork = 3.93874893797439d;
+        char modelOfCPU = 'R';
+        boolean isWindows = true;
+    }
+}

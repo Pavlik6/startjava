f220c660e10bb4bb66d2b698fcde131016a73d16
Pasha Sheholay <pavlik198926@gmail.com>
2020-03-23 19:52:55 +0300

    Add file 'ConditionalStatement'

---
+++ startjava/lesson_1/ConditionalStatement.java
@@ -0,0 +1,37 @@
+class ConditionalStatement {
+    public static void main(String[] args) {
+        int age = 30;
+
+        if(age > 20) {
+            System.out.println("You are welcome!");
+        }
+
+        boolean isMan = true;
+
+        if(isMan) {
+            System.out.println("You are a man!");
+        }
+
+        if(!isMan) {
+            System.out.println("You aren't a man");
+        }
+
+        float height = 1.86f;
+
+        if(height < 1.80) {
+            System.out.println("You are small");
+        } else {
+            System.out.println("You are very height");
+        }
+
+        char firstLetterOfName = 'M';
+
+        if(firstLetterOfName == 'M') {
+            System.out.println("Your name is Max");
+        } else if(firstLetterOfName == 'I') {
+            System.out.println("Your name is Inna");
+        } else {
+            System.out.println("Who are you?");
+        }
+    }
+}

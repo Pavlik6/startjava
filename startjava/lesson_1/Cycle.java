1480422b74e505bf608619d8d5ecbd5caae37721
Pasha Sheholay <pavlik198926@gmail.com>
2020-03-23 20:41:06 +0300

    Add file <Cycle>

---
+++ startjava/lesson_1/Cycle.java
@@ -0,0 +1,30 @@
+class Cycle {
+    public static void main(String[] args) {
+        //Выведите на консоль с помощью цикла for все числа от [0, 20]
+        for(int i = 0; i <= 20; i++) {
+            System.out.println(i);
+        }
+        //Выведите на консоль с помощью цикла while 
+        //все числа от [6, -6] (шаг итерации равен 2)
+        int i = 6;
+
+        while(i >= -6) {
+            System.out.println(i);
+            i -= 2;
+        }
+
+        //Подсчитайте с помощью цикла do-while сумму нечетных чисел
+        // от [10, 20], и выведите ее на консоль
+        int sumOddOfNumbers = 0;
+        int j = 10;
+        
+        do {
+            if(j % 2 != 0) {
+                sumOddOfNumbers += j;
+            }
+            j++;
+        } while(j <= 20);
+
+        System.out.println(sumOddOfNumbers);
+    }
+}

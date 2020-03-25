085e0b2260a2b36d5b89047ec421f138a7fc773f
Pasha Sheholay <pavlik198926@gmail.com>
2020-03-25 20:12:59 +0300

    Add file <Unicode>

---
+++ startjava/lesson_1/Unicode.java
@@ -0,0 +1,9 @@
+// Выведите в цикле символы кодировки Unicode в диапазоне [9398, 10178].
+// Если выводятся кракозябры, то [33, 126]
+class Unicode {
+    public static void main(String[] args) {
+        for(int i = 33; i <= 126; i++) {
+            System.out.println(i + " - " + (char)i);
+        }
+    }
+}
